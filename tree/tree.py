215a7f2fe0e9886316e1f6ee5d70709b866823e4
yujinghui <jinghuiace@gmail.com>
2018-12-08 16:25:30 +0800

    knn

---
+++ tree/tree.py
@@ -0,0 +1,90 @@
+from math import log
+import operator
+
+
+def createData():
+    dataSet = [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
+    label = ['no surfacing', 'flippers']
+    return dataSet, label
+
+
+"""
+    calc shannon entropy base on labels,
+    note that featVect[-1] is label.
+"""
+
+
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCount = {}
+    for featVect in dataSet:
+        currentlabel = featVect[-1]
+        if currentlabel not in labelCount.keys():
+            labelCount[currentlabel] = 0
+        labelCount[currentlabel] = labelCount[currentlabel] + 1
+    shannonEnt = 0.0
+    for key in labelCount:
+        prob = float(labelCount[key] / numEntries)
+        shannonEnt = shannonEnt - prob * log(prob, 2)
+    return shannonEnt
+
+
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis + 1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntropy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = len(subDataSet) / float(len(dataSet))
+            newEntropy = newEntropy + prob * calcShannonEnt(subDataSet)
+        infoGain = baseEntropy - newEntropy
+        if infoGain > bestInfoGain:
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
+
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount.keys():
+            classCount[vote] = 0
+            classCount[vote] = classCount[vote] + 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel: {}}
+    del (labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+
+
---
+++ tree/treePlotter.py
@@ -0,0 +1,67 @@
+import matplotlib.pyplot as plt
+
+decisionNode = dict(boxstyle="sawtooth", fc="0.8")
+leafNode = dict(boxstyle="round4", fc="0.8")
+arrow_args = dict(arrowstyle="<-")
+
+
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction',
+                            xytext=centerPt, textcoords="axes fraction",
+                            va="center", ha="center", bbox=nodeType, arrowprops=arrow_args)
+
+
+def createPlot():
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    createPlot.ax1 = plt.subplot(111, frameon=False)
+    plotNode("decision node", (0.5, 0.1), (0.1, 0.5), decisionNode)
+    plotNode("leaf node", (0.8, 0.1), (0.3, 0.8), leafNode)
+    plt.show()
+
+
+def getNumLeafs(myTree):
+    numLeafs = 0
+    firstStr = list(myTree.keys())[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            numLeafs = numLeafs + getNumLeafs(secondDict[key])
+        else:
+            numLeafs = numLeafs + 1
+    return numLeafs
+
+
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = list(myTree.keys())[0]
+    secondDict = myTree[firstStr]
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            thisDepth = thisDepth + getTreeDepth(secondDict[key])
+        else:
+            thisDepth = 1
+
+        if thisDepth > maxDepth: maxDepth = thisDepth
+    return maxDepth
+
+
+def retrieveTree(i):
+    listOfTree = [{"no surfacing": {0: "no", 1: {
+        'flippers': {0: 'no', 1: "yes"}
+    }}}, {'no surfacing': {0: "no", 1: {"flippers": {
+        0: {"head": {0: "no", 1: "yes"}}, 1: "no"
+    }}}}]
+    return listOfTree[i]
+
+
+def plotMidText(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0] - cntrPt[0])/ 2.0 + cntrPt[0]
+    yMid = (parentPt[1] - cntrPt[1])/ 2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString)
+
+def plotTree(myTree, parentPt, nodeTxt):
+    numLeafs = getNumLeafs(myTree)
+    depth = getTreeDepth(myTree)
+    firstStr = list(myTree.keys())[0]
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs)/2.0/plotTree.totalW, plotTree.yOff))
