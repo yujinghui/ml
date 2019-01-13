e4bcafd5ef4428b1635f9751694e7251e25279a3
yujinghui <jinghuiace@gmail.com>
2019-01-13 09:38:21 +0800

    platt

--- svm/platt.py
+++ svm/platt.py
@@ -1,6 +1,14 @@
 import numpy as np
 
 
+def clipAlpha(aj, H, L):
+    if aj > H:
+        aj = H
+    if L > aj:
+        aj = L
+    return aj
+
+
 class optStruct:
     def __init__(self, dataMatIn, classLabels, C, toler):
         self.X = dataMatIn
@@ -21,13 +29,13 @@ def selectJ(i, oS, Ei):
     maxK = -1
     maxDeltaE = 0
     Ej = 0
-    oS.eCache[i] = [1,Ei]
-    validEcacheList = np.nonzero(oS.eCache[:,0].A)[0]
+    oS.eCache[i] = [1, Ei]
+    validEcacheList = np.nonzero(oS.eCache[:, 0].A)[0]
     if len(validEcacheList) > 1:
         for k in validEcacheList:
             if k == i:
                 continue
-            Ek = calcEk(oS,k)
+            Ek = calcEk(oS, k)
             deltaE = np.abs(Ei - Ek)
             if deltaE > maxDeltaE:
                 maxK = k
@@ -43,3 +51,86 @@ def updateEk(oS, k):
     Ek = calcEk(oS, k)
     oS.eCache[k] = [1, Ek]
 
+
+def innerL(i, oS):
+    Ei = calcEk(oS, i)
+    if (oS.labelMat[i] * Ei < -oS.tol and oS.alphas[i] < oS.C) or \
+            (oS.labelMat[i] * Ei > oS.tol and oS.alphas[i] > 0):
+        j, Ej = selectJ(i, oS, Ei)
+        alphaIold = oS.alphas[i].copy()
+        alphaJold = oS.alphas[j].copy()
+        if oS.labelMat[i] != oS.labelMat[j]:
+            L = max(0, oS.labelMat[j] - oS.labelMat[i])
+            H = min(oS.C, oS.C + oS.labelMat[j] - oS.labelMat[i])
+        else:
+            L = max(0, oS.labelMat[j] + oS.labelMat[i] - oS.C)
+            H = min(oS.C, oS.labelMat[j] + oS.labelMat[i])
+
+        if L == H:
+            print("L == H")
+            return 0
+        eta = 2.0 * oS.X[i, :] * oS.X[j, :].T - \
+              oS.X[i, :] * oS.X[i, :].T - \
+              oS.X[j, :] * oS.X[j, :].T
+        if eta >= 0:
+            print("ets > = 0")
+            return 0
+        oS.alphas[j] -= oS.labelMat[j] * (Ei - Ej) / eta
+        oS.alphas[j] = clipAlpha(oS.alphas[j], H, L)
+        updateEk(oS, j)
+        if abs(oS.alphas[j] - alphaJold) < 0.00001:
+            print("j not moving enought")
+            return 0
+        oS.alphas[i] += oS.labelMat[j] * oS.labelMat[i] * \
+                        (alphaJold - oS.alphas[j])
+        updateEk(oS, i)
+        b1 = oS.b - Ei - oS.labelMat[i] * (oS.alphas[i] - alphaIold) * \
+             oS.X[i, :] * oS.X[i, :].T - oS.labelMat[j] * \
+             (oS.alphas[j] - alphaJold) * oS.X[i, :] * oS.X[j, :].T
+        b2 = oS.b - Ej - oS.labelMat[i] * (oS.alphas[i] - alphaIold) * \
+             oS.X[i, :] * oS.X[j, :].T - oS.labelMat[j] * \
+             (oS.alphas[j] - alphaJold) * oS.X[j, :] * oS.X[j, :].T
+        if 0 < oS.alphas[i] < oS.C:
+            oS.b = b1
+        elif 0 < oS.alphas[j] < oS.C:
+            oS.b = b2
+        else:
+            oS.b = (b1 + b2) / 2.0
+        return 1
+    else:
+        return 0
+
+
+def smoP(dataMatIn, classLabels, C, toler, maxIter, kTup=('lin', 0)):
+    oS = optStruct(np.mat(dataMatIn), np.mat(classLabels).transpose(), C, toler)
+    iter = 0
+    entireSet = True
+    alphaPairsChanged = 0
+    while (iter < maxIter and alphaPairsChanged > 0) or entireSet:
+        alphaPairsChanged = 0
+        if entireSet:
+            for i in range(oS.m):
+                alphaPairsChanged = alphaPairsChanged + innerL(i, oS)
+                print("fullSet, iter : %d i: %d , pair changed %d" % (iter, i, alphaPairsChanged))
+            iter = iter + 1
+        else:
+            nonBoundIs = np.nonzero((oS.alphas.A > 0) * (oS.alphas.A < C))[0]
+            for i in nonBoundIs:
+                alphaPairsChanged = alphaPairsChanged + innerL(i, oS)
+                print("no bounds iter %d i : %d, pair changed %d " % (iter, i, alphaPairsChanged))
+            iter = iter + 1
+        if entireSet:
+            entireSet = False
+        elif alphaPairsChanged == 0:
+            entireSet = True
+    return oS.b, oS.alphas
+
+
+def calcWs(alphas, dataArr, classLabels):
+    X = np.mat(dataArr)
+    labelMat = np.mat(classLabels).transpose()
+    m, n = np.shape(X)
+    w = np.zeros((n, 1))
+    for i in range(m):
+        w = w + np.multiply(alphas[i] * labelMat[i], X[i, :].T)
+    return w
