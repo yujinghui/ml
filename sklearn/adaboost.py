5f6590d02eb969cc66e0fbbd4df3bf3a11463bdd
yujinghui <yujinghui@huya.com>
2019-01-29 18:47:13 +0800

    feature: requirements.txt
    1. reqirements.txt

---
+++ sklearn/adaboost.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+# importing necessary libraries
+import numpy as np
+from sklearn import metrics
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.tree import DecisionTreeRegressor
