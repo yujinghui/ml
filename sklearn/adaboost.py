d8deda52a003019fe74ec64f014aa9d77f975874
yujinghui <yujinghui@huya.com>
2019-01-30 09:50:59 +0800

    feature: sklearn adaboost
    1. sklearn adaboost

--- sklearn/adaboost.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-# importing necessary libraries
-import numpy as np
-from sklearn import metrics
-from sklearn.ensemble import AdaBoostRegressor
-from sklearn.tree import DecisionTreeRegressor
--- sklearndemo/nativeBayes.py
+++ sklearndemo/nativeBayes.py
@@ -1,6 +1,6 @@
 import numpy as np  
 X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])  
 Y = np.array([1, 2, 3, 4, 5, 6])  
-from sklearn.naive_bayes import GaussianNB  
+from sklearn.naive_bayes import GaussianNB
 clf = GaussianNB().fit(X, Y)  
 print (clf.predict([[-0.8,-1]])  )
