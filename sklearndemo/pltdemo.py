b35ebed1ac77b1c1ad795af20d8261d79981634b
yujinghui <yujinghui@huya.com>
2019-02-02 14:11:00 +0800

    feature: plt 3d demo
    1. plt 3d demo

--- sklearndemo/pltdemo.py
+++ sklearndemo/pltdemo.py
@@ -1,58 +1,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
-class My2DCoordination:
-
-    def __init__(self, xrange, yrange):
-        """
-        初始化坐标范围.
-        :param xrange:
-        :param yrange:
-        """
-        self.plt = plt
-        self.plt.figure()
-        self.ax = plt.gca()
-        self.ax.set_xlim([0, xrange])
-        self.ax.set_ylim([0, yrange])
-        self.x = []
-        self.y = []
-
-    def add_2d_vect(self, x, y):
-        """
-        为了更方便的理解矩阵运算，使用python matplotlib画出向量.
-        :return:
-        """
-        self.x.append(x)
-        self.y.append(y)
-        self.ax.quiver((0,) * len(self.x), (0,) * len(self.y), self.x, self.y, angles='xy', scale_units='xy', scale=1)
-
-    def add_all_2d_vect(self, x, y):
-        """
-        为了更方便的理解矩阵运算，使用python matplotlib画出向量.
-        :return:
-        """
-        self.x.extend(x)
-        self.y.extend(y)
-        self.ax.quiver((0,) * len(self.x), (0,) * len(self.y), self.x, self.y, angles='xy', scale_units='xy', scale=1)
-
-    def show(self):
-        self.plt.draw()
-        self.plt.show()
-
-    def multi_matrix(self, matrix):
-        x = []
-        y = []
-        data_mat = np.mat(matrix)
-        for item in zip(self.x, self.y):
-            res = np.dot(data_mat, np.array(item))
-            x.append(res.item((0, 0)))
-            y.append(res.item((0, 1)))
-        return x, y
+from matplotlib import cm
+from mpl_toolkits.mplot3d import axes3d
 
 
-if __name__ == '__main__':
-    coord = My2DCoordination(10, 10)
-    coord.add_2d_vect(3, 4)
-    x, y = coord.multi_matrix([[7, 2], [9, 5]])
-    coord.add_all_2d_vect(x, y)
-    coord.show()
+def func(x1, x2):
+    return x1 **2+ x2**2
+
+
+X = np.arange(-10, 10, 0.1)
+Y = np.arange(-10, 10, 0.1)
+X, Y = np.meshgrid(X, Y)
+Z = func(X, Y)
+
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+ax.plot_surface(X, Y, Z, rstride=8, cstride=8, alpha=0.3)
+cset = ax.contour(X, Y, Z, zdir='z', offset=-100, cmap=cm.coolwarm)
+cset = ax.contour(X, Y, Z, zdir='x', offset=-40, cmap=cm.coolwarm)
+cset = ax.contour(X, Y, Z, zdir='y', offset=40, cmap=cm.coolwarm)
+
+ax.set_xlabel('X')
+ax.set_xlim(-20, 20)
+ax.set_ylabel('Y')
+ax.set_ylim(-20, 20)
+ax.set_zlabel('Z')
+ax.set_zlim(0, 20)
+
+plt.show()
